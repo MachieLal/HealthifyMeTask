edf6d550485ea5416c07f41e2c3889894ee84be6
SwarupPattnaik <swarup.pattnaik@evry.com>
2017-05-09 22:10:29 +0530

    Initial Commit

---
+++ HealthifyMeTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthifyMeTask
+//
+//  Created by Swarup-Pattnaik on 5/9/17.
+//  Copyright © 2017 Swarup-Pattnaik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
